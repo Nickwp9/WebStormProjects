1f77a643f1e6b3ec31f383cf8eca9f28b77d8a64
Nickwp9 <nicucernei47@gmail.com>
2026-09-21 11:09:44 +0300

    Path added

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,7 +1,10 @@
 import { HomePage } from '../pages/HomePage'
 import { ProductPage} from '../pages/ProductPage'
 import { CartPage } from '../pages/CartPage'
+import { createBrowserRouter } from 'react-router-dom'
 
 const router = createBrowserRouter([
-
+    { path: '/home', element: <HomePage /> }
+    { path: '/product', element: <ProductPage /> }
+    { path: '/cart', element: <CartPage /> }
 ]);
