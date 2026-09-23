c21fd308824527d70cd4be9a9baa7eea594350b1
Nickwp9 <nicucernei47@gmail.com>
2026-09-23 09:07:14 +0300

    Added logo
    Still has to be tweaked tho

--- frontend/src/layout/AppLayout.tsx
+++ frontend/src/layout/AppLayout.tsx
@@ -1,10 +1,10 @@
 import { Outlet } from 'react-router-dom';
-
+import logo from '../assets/Logo with text.jpg';
 export const AppLayout: React.FC = () => {
   return (
     <div>
       <header>
-
+        <img src = {logo} alt = "Crossover"/>
       </header>
       <Outlet />
     </div>
--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -2,10 +2,16 @@ import { HomePage } from '../pages/HomePage'
 import { ProductPage} from '../pages/ProductPage'
 import { CartPage } from '../pages/CartPage'
 import { createBrowserRouter } from 'react-router-dom'
+import { AppLayout } from '../layout/AppLayout'
 
 export const router = createBrowserRouter([
-    { path: '/', element: <HomePage /> },
-    { path: '/product/:slug', element: <ProductPage /> },
-    { path: '/cart', element: <CartPage /> }
+    {path: '/',
+      element: <AppLayout />,
+      children:[
+    { index: true, element: <HomePage /> },
+    { path: 'product/:slug', element: <ProductPage /> },
+    { path: 'cart', element: <CartPage /> }
+    ]
+  }
 ]);
 
