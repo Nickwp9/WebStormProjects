bc3bd0cb31f2e72b9861bc016d9d06e3b188297c
Nickwp9 <nicucernei47@gmail.com>
2026-09-23 11:12:56 +0300

    added container

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -3,7 +3,7 @@ import bannerVideo from '../assets/230749.mp4'
 export const HomePage: React.FC = () => {
   return (
     <section className="banner">
-      <video className="banner-video" autoPlay loop muted playsInline>
+      <video className="banner-video" autoPlay loop muted playsInline style = {{pointerEvents: 'none'}}>
         <source src={bannerVideo} type="video/mp4" />
       </video>
       <div className="banner-content">
