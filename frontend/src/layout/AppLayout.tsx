fe3ff5f01d20febd7eca0f935acd0b391c576944
Nickwp9 <nicucernei47@gmail.com>
2026-09-23 10:52:05 +0300

    Added a video as a background for the homepage
    The video still has to be twearked and I will make it 2-3% opacity

--- frontend/src/layout/AppLayout.tsx
+++ frontend/src/layout/AppLayout.tsx
@@ -1,10 +1,15 @@
-import { Outlet } from 'react-router-dom';
-import logo from '../assets/Logo with text.jpg';
+import { Outlet, Link } from 'react-router-dom';
+import logo from '../assets/Logo without text.jpg';
 export const AppLayout: React.FC = () => {
   return (
     <div>
-      <header>
-        <img src = {logo} alt = "Crossover"/>
+      <header className='header'>
+      <Link to = '/'>
+        <img src = {logo} alt = "Crossover" style = {{height: '72px', width: 'auto'}}/>
+       </Link>
+        <nav className='nav'>
+          <Link to = '/cart'>Cart</Link>
+        </nav>
       </header>
       <Outlet />
     </div>
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,5 +1,13 @@
+import bannerVideo from '../assets/230749.mp4'
+
 export const HomePage: React.FC = () => {
   return (
-    <div>Home page</div>
+    <section className="banner">
+      <video className="banner-video" autoPlay loop muted playsInline>
+        <source src={bannerVideo} type="video/mp4" />
+      </video>
+      <div className="banner-content">
+      </div>
+    </section>
   );
 };
